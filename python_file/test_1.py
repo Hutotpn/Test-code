930cc52eef39755e436c41a9c9d9f69a23b5611b
Hutotpn <92351427+Hutotpn@users.noreply.github.com>
2022-08-02 18:47:55 +0700

    Update test_1.py

--- python_file/test_1.py
+++ python_file/test_1.py
@@ -1,5 +1,8 @@
-name = input("Enter name:")
-email = input("Enter email:")
+def ask():
+  name = input("Enter name:")
+  email = input("Enter email:")
+
+ask()
 print("Welcome " + name)
 print("Your email is " + email)
 yes = input("Type y to exit ")
